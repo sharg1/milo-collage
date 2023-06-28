d3fd3f5a56e9863f536563310be98925a7c25d40
Sharath G <sharg@adobe.com>
2023-06-28 13:26:09 +0530

    Update blog-list.js

--- blocks/blog-list/blog-list.js
+++ blocks/blog-list/blog-list.js
@@ -7,14 +7,16 @@ export default async function init(el) {
   const list = createTag('ul', { class: 'blog-list-container' });
   const qJson = await qResp.json();
   qJson.data.forEach(async (post) => {
+    const li = createTag('li', { class: 'blog-list-item' });
+    list.append(li);
     const resp = await fetch(`${post.path}.plain.html`);
     if (!resp.ok) return;
     const html = await resp.text();
     const parser = new DOMParser();
     const doc = parser.parseFromString(html, 'text/html');
-    const heading = doc.querySelector('h1, h2, h3, h4, h5, p');
-    const li = createTag('li', { class: 'blog-list-item' }, heading);
-    list.append(li);
+    const heading = doc.querySelector('h1, h2, h3, h4, h5, p').textContent;
+    const h2 = createTag('h2', {}, heading);
+    li.append(h2);
   });
   el.append(list);
 }
